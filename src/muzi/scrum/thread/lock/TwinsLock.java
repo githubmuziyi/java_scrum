349ab08510d6ba240f3146291742444117ba4984
muzi.he <muzi.habo@gmail.com>
2018-07-23 22:52:12 +0800

    只允许同时两个线程并发访问的锁

---
+++ src/muzi/scrum/thread/lock/TwinsLock.java
@@ -0,0 +1,110 @@
+package muzi.scrum.thread.lock;
+
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.locks.AbstractQueuedSynchronizer;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+
+/**
+ * 只允许同时两个线程并发访问的锁
+ */
+public class TwinsLock implements Lock {
+
+    private static Sync sync = new Sync(2);
+
+    private static final class Sync extends AbstractQueuedSynchronizer {
+
+        Sync(int count) {
+            if (count <= 0) {
+                throw new IllegalArgumentException("count must large than zero");
+            }
+            setState(count);
+        }
+
+        @Override
+        protected int tryAcquireShared(int reduceCount) {
+            for (;;) {
+                int current = getState();
+                int newCount = current - reduceCount;
+                if (newCount < 0 || compareAndSetState(current, newCount)) {
+                    return newCount;
+                }
+            }
+        }
+
+        @Override
+        protected boolean tryReleaseShared(int returnCount) {
+            for (;;) {
+                int current = getState();
+                int newCount = current + returnCount;
+                if (compareAndSetState(current, newCount)) {
+                    return true;
+                }
+            }
+        }
+    }
+
+    @Override
+    public void lock() {
+        sync.tryAcquireShared(1);
+    }
+
+    @Override
+    public void lockInterruptibly() throws InterruptedException {
+
+    }
+
+    @Override
+    public boolean tryLock() {
+        return false;
+    }
+
+    @Override
+    public boolean tryLock(long time, TimeUnit unit) throws InterruptedException {
+        return false;
+    }
+
+    @Override
+    public void unlock() {
+        sync.tryReleaseShared(1);
+    }
+
+    @Override
+    public Condition newCondition() {
+        return null;
+    }
+
+    // test
+    public static void main(String[] args) throws InterruptedException {
+
+        final Lock lock = new TwinsLock();
+
+        class Worker extends Thread {
+            @Override
+            public void run() {
+                while (true) {
+                    lock.lock();
+                    try {
+                        TimeUnit.SECONDS.sleep(1);
+                        System.out.println(Thread.currentThread().getName());
+                        TimeUnit.SECONDS.sleep(1);
+                    } catch (InterruptedException e) {
+                    } finally {
+                        lock.unlock();
+                    }
+                }
+            }
+        }
+
+        for (int i = 0; i < 10; i++) {
+            Worker worker = new Worker();
+            worker.setDaemon(true);
+            worker.start();
+        }
+
+        for (int i = 0; i < 10; i++) {
+            TimeUnit.SECONDS.sleep(1);
+            System.out.println();
+        }
+    }
+}
